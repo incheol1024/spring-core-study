d8724ec34babf26d0a75fa3fe763f9bb08a78f0f
miyoung kim <miyoungkim@miyoungui-MacBookAir.local>
2020-03-09 00:15:18 +0900

    aop추가

---
+++ src/main/java/me/spring/core/aop/MyAop.java
@@ -0,0 +1,12 @@
+package me.spring.core.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyAop {
+
+    
+
+}
